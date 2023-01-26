88ed08d66140037197467ecdcbeed071f234a68c
365support <86206374+365support@users.noreply.github.com>
2023-01-26 19:13:18 +0900

    chore: init

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const persistor = persistStore(store);
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-    <PersistGate persistor={persistor}>
-        <App />
-    </PersistGate>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<QueryClientProvider client={queryClient}>
+				<PersistGate persistor={persistor}>
+					<App />
+					<ReactQueryDevtools initialIsOpen={false} />
+				</PersistGate>
+			</QueryClientProvider>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
 
 // If you want to start measuring performance in your app, pass a function
